7139337fa462787c4ea26e05115885e69eff6f6b
Sravanthi931 <ksravs917@gmail.com>
2025-06-01 19:48:31 +0530

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import Product from "./components/Product";
 import Cart from "./components/Cart";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
+import Logout from "./components/Logout";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createContext } from "react";
+export const AppContext = createContext();
 function App() {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
   return (
     <div>
-      <BrowserRouter>
-        <header>
-          <h1>My Online Shop..</h1>
-          <Link to="/">Home</Link> - 
-          <Link to="/cart">Cart</Link> - 
-          <Link to="/login">Login</Link>
-          <hr />
-        </header>
-
-        <main>
+      <AppContext.Provider value={{ users, setUsers, user, setUser }}>
+        <BrowserRouter>
+          <Header />
           <Routes>
             <Route index element={<Product />} />
-            <Route path="/" element={<Product />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Product />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/logout" element={<Logout />}></Route>
+            <Route path="/register" element={<Register />}></Route>
           </Routes>
-        </main>
-
-        <footer>
-          <hr />
-          &copy; 2005. All rights Reserved.
-        </footer>
-      </BrowserRouter>
+          <Footer />
+        </BrowserRouter>
+      </AppContext.Provider>
     </div>
   );
 }
-
 export default App;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { AppContext } from "../App";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-
 export default function Register() {
-  const navigate = useNavigate();
-
+  const { users, setUsers } = useContext(AppContext);
+  const [user, setUser] = useState({});
+const Navigate = useNavigate()
+  const handleSubmit = () => {
+    setUsers([...users, user]);
+    Navigate("/login")
+  };
   return (
-    <div>
-      <h2>Register</h2>
-      <input type="text" placeholder="Name" />
-      <br />
-      <input type="email" placeholder="Email" />
-      <br />
-      <input type="password" placeholder="Password" />
-      <br />
-      <button onClick={() => navigate("/")}>Submit</button>
+    <div style={{ margin: "30px" }}>
+      <h3>Register</h3>
+      <p>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+        />
+      </p>
+      <p>
+        <input
+          type="text"
+          placeholder="Email address"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+      </p>
+      <p>
+        <input
+          type="password"
+          placeholder="New Password"
+          onChange={(e) => setUser({ ...user, pass: e.target.value })}
+        />
+      </p>
+      <button onClick={handleSubmit}>Submit</button>
+      <hr />
+      {users && users.map(value=>(
+        <li>{value.name}-{value.email}-{value.pass}</li>
+      ))}
     </div>
   );
 }
